0e6ca8e34f31ac4074e4c1654a877d87a12ad4b7
Katarzyna Wicherska <kawi@gft.com>
2017-03-02 15:09:13 +0100

    added paying function, TODO: change code to object oriantation

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -9,7 +9,7 @@ enum VendingMachineSize {
 }
 
 class Cell {
-    constructor (public product: CocaCola){
+    constructor(public product: CocaCola) {
 
     }
     stock = ko.observable(3);
@@ -18,18 +18,39 @@ class Cell {
 
 class VendingMachine {
     paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()));
     cells = ko.observableArray([]);
-
-set size(giveSize: VendingMachineSize){
-    for(let index = 0; index < giveSize; index++){
-        let product = productFactory.getProduct();
-        this.cells.push(new Cell(product));
-    }
-}
-
     acceptedCoins: Quarter[] = [new Quarter()];
+    // pureComputer will calculate automatically when one of the observable metioned in the expression changes
+    canPay = ko.pureComputed(() => this.paid() - this.selectedCell().product.price >= 0);
+
+
+    set size(giveSize: VendingMachineSize) {
+        for (let index = 0; index < giveSize; index++) {
+            let product = productFactory.getProduct();
+            this.cells.push(new Cell(product));
+        }
+    }
+
+    select = (cell: Cell): void => {
+        cell.sold(false);
+        this.selectedCell(cell);
+    }
+
     acceptCoin = (coin: Quarter): void => {
         let oldTotal = this.paid();
         this.paid(oldTotal + coin.value);
+    }
+
+    pay = (): void => {
+        if (this.selectedCell().stock() < 1) {
+            alert("I'm sorry, we are out of that product!");
+            return;
+        }
+        let currentPaid = this.paid();
+        this.paid(Math.round(((currentPaid - this.selectedCell().product.price) * 100)) / 100);
+        let currentStock = this.selectedCell().stock();
+        this.selectedCell().stock(currentStock - 1);
+        this.selectedCell().sold(true);
     }
 }
